4c576c72c37871fed6a3217743b4badf0f811a59
Noor Muhammad <connectwithnoor1@gmail.com>
2020-07-27 15:12:42 +0500

    add server.js for heroku deployment

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+const publicPath = path.join(__dirname, 'build');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log('Server is up!');
+});

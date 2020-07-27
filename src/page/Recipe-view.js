1e3507a829b3b1f6fca9dbbd75fe07908429853e
Noor Muhammad <connectwithnoor1@gmail.com>
2020-07-27 12:16:44 +0500

    recipe ing page

--- src/page/Recipe-view.js
+++ src/page/Recipe-view.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const RecipeView = () => {
-  return <div>RecipeView</div>;
+  return (
+    <div className='recipe-view'>
+      <div className='img-wrapper'>
+        <img
+          className='recipe-img'
+          src={require('../assets/images/smoothie.png')}
+          alt='smoothie'
+        />
+      </div>
+
+      <p className='recipe-title'>Bablish Apple Pie Smoothie</p>
+
+      <ul className='recipe-ingredients'>
+        <li>1 - Almond</li>
+        <li>2 - Almond</li>
+        <li>3 - Almond</li>
+        <li>4 - Almond</li>
+      </ul>
+    </div>
+  );
 };
 
 export default RecipeView;
